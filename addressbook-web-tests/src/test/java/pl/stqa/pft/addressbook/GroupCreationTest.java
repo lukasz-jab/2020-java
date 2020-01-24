6b9cfa91019ccfd00c88c76cb4bf09928287d4c4
lukasz-jab <lukasz.j.pl@interia.pl>
2020-01-24 20:56:16 +0100

    Fixtures for the testGroupCreation

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupCreationTest.java
@@ -15,27 +15,18 @@ public class GroupCreationTest {
     public void setUp() {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        wd.get("http://127.0.0.1/addressbook/");
+       login();
     }
 
     @Test
     public void testGroupCreation() {
-        wd.get("http://127.0.0.1/addressbook/");
-        wd.findElementByCssSelector("form#LoginForm input[name = 'user']").sendKeys("admin");
-        wd.findElementByCssSelector("form#LoginForm input[name = 'pass']").sendKeys("secret");
-        wd.findElementByCssSelector("form#LoginForm input[value = 'Login']").click();
 
-        wd.findElementByCssSelector("div#nav a[href = 'group.php']").click();
-        wd.findElementByCssSelector("div#container div#content input[name = 'new']").click();
-        wd.findElementByCssSelector("div#container div#content input[name = 'group_name']")
-                .sendKeys("new group 23.01.2020");
-        wd.findElementByCssSelector("div#container div#content textarea[ name= 'group_header']")
-                .sendKeys("new group HEADER HEADER HEADER 23.01.2020");
-        wd.findElementByCssSelector("div#container div#content textarea[ name= 'group_footer']")
-                .sendKeys("new group FOOTER FOOTER FOOTER 23.01.2020");
-
-        wd.findElementByCssSelector("div#container div#content input[name = 'submit']").click();
-
-        wd.findElementByCssSelector("div#nav a[href = 'group.php']").click();
+        gotoGroupPage();
+        fillGroupForm();
+        submitGroupForm();
+        returnGroupPage();
     }
 
     @AfterTest
@@ -43,4 +34,34 @@ public class GroupCreationTest {
         wd.quit();
         wd=null;
     }
+
+    public void login() {
+        wd.findElementByCssSelector("form#LoginForm input[name = 'user']").sendKeys("admin");
+        wd.findElementByCssSelector("form#LoginForm input[name = 'pass']").sendKeys("secret");
+        wd.findElementByCssSelector("form#LoginForm input[value = 'Login']").click();
+    }
+
+    public void gotoGroupPage() {
+        wd.findElementByCssSelector("div#nav a[href = 'group.php']").click();
+    }
+
+    public void fillGroupForm() {
+        wd.findElementByCssSelector("div#container div#content input[name = 'new']").click();
+        wd.findElementByCssSelector("div#container div#content input[name = 'group_name']")
+                .sendKeys("new group 23.01.2020");
+        wd.findElementByCssSelector("div#container div#content textarea[ name= 'group_header']")
+                .sendKeys("new group HEADER HEADER HEADER 23.01.2020");
+        wd.findElementByCssSelector("div#container div#content textarea[ name= 'group_footer']")
+                .sendKeys("new group FOOTER FOOTER FOOTER 23.01.2020");
+    }
+
+    public void submitGroupForm() {
+        wd.findElementByCssSelector("div#container div#content input[name = 'submit']").click();
+    }
+
+    public void returnGroupPage() {
+        wd.findElementByCssSelector("div#nav a[href = 'group.php']").click();
+    }
+
+
 }
