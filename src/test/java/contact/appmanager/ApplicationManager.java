3ec3c08e5d1fdd587718c3376fd08b8da82f27ce
lukasz-jab <lukasz.j.pl@interia.pl>
2020-02-25 14:28:44 +0100

    Create local.properties

--- src/test/java/contact/appmanager/ApplicationManager.java
+++ src/test/java/contact/appmanager/ApplicationManager.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.*;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+
+    Properties properties;
 
     private WebDriver wd;
     private String browser;
@@ -18,23 +22,27 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
 
+        String target = System.getProperty("target","local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target) )));
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         }
-        if (browser.equals((BrowserType.CHROME))) {
+        if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
         }
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wd.get("http://127.0.0.1/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl","http://127.0.0.1/addressbook/"));
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         // helperBase = new HelperBase(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin","admin"),
+                properties.getProperty("web.adminPass", "secret"));
 
 
     }
--- src/test/java/contact/tests/TestBase.java
+++ src/test/java/contact/tests/TestBase.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+
 public class TestBase {
 
-    static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
     }
 
