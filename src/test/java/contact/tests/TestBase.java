1809f3be0cfa10f1cf85045273731285b238eb36
lukasz-jab <lukasz.j.pl@interia.pl>
2020-02-25 16:00:47 +0100

    Added Logback library

--- src/test/java/contact/tests/TestBase.java
+++ src/test/java/contact/tests/TestBase.java
@@ -2,14 +2,24 @@ package contact.tests;
 
 import contact.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
+
     static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp() throws IOException {
@@ -17,10 +27,22 @@ public class TestBase {
     }
 
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void TearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void testLogStart(Method m, Object[] p) {
+        logger.info("Start tests " + m.getName() + " with parameters: " +Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void testLogStop(Method m, Object[] p) {
+        logger.info("Stop Contact creation test" + m.getName() + Arrays.asList(p));
+    }
 
 }
+
+
+
